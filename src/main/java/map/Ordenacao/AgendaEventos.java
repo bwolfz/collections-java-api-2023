92bfa99496693984cc136325326bd403dbbc8fd4
bwolfz <kildertavora@gmail.com>
2024-05-23 15:49:21 -0300

    Map Pesquisa update

---
+++ src/main/java/map/Ordenacao/AgendaEventos.java
@@ -0,0 +1,50 @@
+package main.java.map.Ordenacao;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class AgendaEventos {
+    //atributo
+    private Map<LocalDate, Evento> eventosMap;
+
+    public AgendaEventos() {
+        this.eventosMap = new HashMap<>();
+    }
+
+    public void adicionarEvento(LocalDate data, String nomeEvento, String descricaoAtracao) {
+        eventosMap.put(data, new Evento(nomeEvento, descricaoAtracao));
+    }
+
+    public void exibirAgenda(){
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+        System.out.println(eventosTreeMap);
+    }
+
+    public void obterProximoEvento(){
+        LocalDate dataAtual = LocalDate.now();
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+        for(Map.Entry<LocalDate, Evento> entry : eventosMap.entrySet()){
+            if(entry.getKey().isEqual(dataAtual) || entry.getKey().isAfter(dataAtual)){
+                System.out.println("O próximo evento é "+entry.getValue() + " na data "+entry.getKey());
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        AgendaEventos agendaEventos = new AgendaEventos();
+
+        agendaEventos.adicionarEvento(LocalDate.of(2022, Month.JULY, 15), "Conferência de Tecnologia", "Palestrante renomado");
+        agendaEventos.adicionarEvento(LocalDate.of(2022, 7, 9), "Workshop de Programação", "Aula prática de desenvolvimento");
+        agendaEventos.adicionarEvento(LocalDate.of(2000, 1, 10), "Lançamento de Software", "Demonstração da nova versão");
+        agendaEventos.adicionarEvento(LocalDate.of(2023, Month.AUGUST, 28), "Hackathon de Inovação", "Competição de soluções criativas");
+        agendaEventos.adicionarEvento(LocalDate.of(2024, 9, 20), "Seminário de Inteligência Artificial", "Discussão sobre IA avançada");
+
+        agendaEventos.exibirAgenda();
+        agendaEventos.obterProximoEvento();
+
+    }
+}
